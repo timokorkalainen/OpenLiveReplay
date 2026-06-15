0d0eca2e704e78cfbf4c845544857d2e2c26b701
Timo Korkalainen <timo@timokorkalainen.net>
2026-06-15 20:58:20 +0300

    test(telemetry): cover SSE parser regressions

--- tests/unit/tst_sseparser.cpp
+++ tests/unit/tst_sseparser.cpp
@@ -7,10 +7,13 @@ class TestSseParser : public QObject {
     Q_OBJECT
 private slots:
     void parsesSingleJsonDataEvent();
+    void joinsMultipleDataLinesWithNewline();
     void parsesChunkedEvent();
     void ignoresCommentsAndKeepsEventType();
+    void ignoresBlocksWithoutDataField();
     void rejectsMalformedJson();
     void rejectsOversizedIncompleteBuffer();
+    void rejectsOversizedCompleteEvent();
     void inheritsLastEventIdAcrossEvents();
     void resetClearsLastEventId();
     void rejectsEmptyDataField();
@@ -38,6 +41,20 @@ void TestSseParser::parsesSingleJsonDataEvent() {
     QCOMPARE(events[0].payload.value("values").toObject().value("batteryPercent").toInt(), 91);
 }
 
+void TestSseParser::joinsMultipleDataLinesWithNewline() {
+    SseParser parser;
+    const QList<TelemetryEvent> events = parser.push(
+        "event: telemetry\n"
+        "data: {\"feedId\":\"cam-main\",\n"
+        "data: \"status\":\"ok\",\"values\":{\"batteryPercent\":91}}\n"
+        "\n");
+
+    QCOMPARE(events.size(), 1);
+    QCOMPARE(events[0].feedId, QStringLiteral("cam-main"));
+    QCOMPARE(events[0].payload.value("status").toString(), QStringLiteral("ok"));
+    QCOMPARE(events[0].payload.value("values").toObject().value("batteryPercent").toInt(), 91);
+}
+
 void TestSseParser::parsesChunkedEvent() {
     SseParser parser;
     QVERIFY(parser.push("data: {\"feed").isEmpty());
@@ -59,6 +76,18 @@ void TestSseParser::ignoresCommentsAndKeepsEventType() {
     QCOMPARE(events[0].lastEventId, QStringLiteral("42"));
 }
 
+void TestSseParser::ignoresBlocksWithoutDataField() {
+    SseParser parser;
+    const QList<TelemetryEvent> events = parser.push(
+        ": heartbeat\n"
+        "event: telemetry\n"
+        "id: 42\n"
+        "\n");
+
+    QVERIFY(events.isEmpty());
+    QVERIFY(parser.lastError().isEmpty());
+}
+
 void TestSseParser::rejectsMalformedJson() {
     SseParser parser;
     const QList<TelemetryEvent> events = parser.push("data: { nope }\n\n");
@@ -69,6 +98,17 @@ void TestSseParser::rejectsMalformedJson() {
 void TestSseParser::rejectsOversizedIncompleteBuffer() {
     SseParser parser;
     const QList<TelemetryEvent> events = parser.push(QByteArray(1024 * 1024 + 1, 'x'));
+    QVERIFY(events.isEmpty());
+    QVERIFY(parser.lastError().contains(QStringLiteral("SSE buffer exceeded")));
+}
+
+void TestSseParser::rejectsOversizedCompleteEvent() {
+    SseParser parser;
+    QByteArray chunk("data: ");
+    chunk.append(QByteArray(1024 * 1024 + 1, 'x'));
+    chunk.append("\n\n");
+
+    const QList<TelemetryEvent> events = parser.push(chunk);
     QVERIFY(events.isEmpty());
     QVERIFY(parser.lastError().contains(QStringLiteral("SSE buffer exceeded")));
 }
