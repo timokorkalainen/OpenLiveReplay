77968bbd5fe9efbb84167cc02e46023433bdb940
Timo Korkalainen <timo@timokorkalainen.net>
2026-06-16 21:50:20 +0300

    feat: adapt websocket api to ui manager

---
+++ websocket/uimanagercontroladapter.h
@@ -0,0 +1,31 @@
+#ifndef UIMANAGERCONTROLADAPTER_H
+#define UIMANAGERCONTROLADAPTER_H
+
+#include "controlapiadapter.h"
+
+#include <QObject>
+
+class UIManager;
+
+class UIManagerControlAdapter : public QObject, public ControlApiAdapter {
+    Q_OBJECT
+public:
+    explicit UIManagerControlAdapter(UIManager *uiManager, QObject *parent = nullptr);
+
+    RecordingState recordingState() const override;
+    TransportState transportState() const override;
+    QVector<SourceState> sourceStates() const override;
+    ViewState viewState() const override;
+    SettingsState settingsState() const override;
+    MidiState midiState() const override;
+    StreamDeckState streamDeckState() const override;
+    ScreensState screensState() const override;
+    ImportState importState() const override;
+    TelemetryState telemetryState() const override;
+    CommandResult executeCommand(const QString &name, const QJsonObject &args) override;
+
+private:
+    UIManager *m_uiManager = nullptr;
+};
+
+#endif
